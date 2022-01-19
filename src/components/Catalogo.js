96b023f47bf5510abafffd895817d25f4cfc0d07
Ricardo Robert <46797052+Ricardoalmeida54@users.noreply.github.com>
2022-01-18 21:12:25 -0300

    ATUALIZADO

--- src/components/Catalogo.js
+++ src/components/Catalogo.js
@@ -1,17 +1,33 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-const Catalogo = () => (
+// O FILTER CRIA UM ARRAY DA CATEGORIA FRONTEND DE LIVROS 
+// MAPEAMOS OS LIVROS FILTRADOS PARA A CATEGORIA FRONTEND
+
+const Catalogo = ({ livros }) => {
+  return (
   <main className="principal">
-    <h2>Catálogo de livros</h2>
-    <ul>
-      <li>
-        <a href="#">Livro 1</a>
-      </li>
-      <li>
-        <a href="#">Livro 2</a>
-      </li>
-    </ul>
+    <h2>Categoria frontend</h2>
+    <ol>
+      {livros
+      .filter(livro => livro.categoria === "frontend")
+      .map(livro => (
+        <li key={livro.id}>
+          <Link to={`/livro/${livro.slug}`}>{livro.titulo}</Link>
+        </li>
+      ))}
+    </ol>
+    <h2>Categoria programação</h2>
+    <ol>
+      {livros
+      .filter(livro => livro.categoria === "programacao")
+      .map(livro => (
+        <li key={livro.id}>
+          <Link to={`/livro/${livro.slug}`}>{livro.titulo}</Link>
+        </li>
+      ))}
+    </ol>
   </main>
 );
-
+};
 export default Catalogo;
